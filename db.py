f6e400108a4382881e97842a16ae461f0c2e226b
Persis Charles <pcharles@ofsoptics.com>
2019-02-05 11:11:57 -0500

    Update db.py
    PF_TENSION - avoid outliers during taping

--- db.py
+++ db.py
@@ -60,39 +60,39 @@ def pf_sample():#PF sample samples all data from the start
 	#manual presssing of start is differentiated by the length of the fiber. if the length is NOT 0 or at least greater than one and then the system receieves a start signal, only then send it to PTS. 
 	#this is temp fix until we can differentiate auto start from manual HMI start. 
 	#
-	#automatic taping START - when taping is done and machine running signal comes on, only then send the start string to PTS 
-	#'Path/mach_start' goes high for five seconds and goes low during semi-automatic, manual, or initial sequence. 
-
-	#set 'Path/mach_start_after_taping' to 1. Use this tag to trigger mach_Stop. 
+	#
 	
-	if (system.tag.read('Path/TU/tapingDone').value == True and system.tag.read('Path/mach_running').value ==True) or (system.tag.read('Path/mach_start').value ==True):
+	if (system.tag.read('Path/TU/tapingDone').value == True and system.tag.read('Path/mach_running').value ==True) or ((system.tag.read('Path/mach_start').value ==True) and (system.tag.read('Path/mach_running').value==True)):
 		shared.mach_start_stop.mach_start() #send to PTS the start string
-		system.tag.write('Path/mach_start_after_taping',1)
+		tapingDone = True
+		system.tag.write('Path/mach_start_after_taping',1) #this value is set to 0 at CompleteTU event.
 		
 	
 	#test_mach_running
 	#while (system.tag.read('Path/test_mach_running').value == 1) and not (system.tag.read('Path/test_mach_stopped').value==1):
-	while  (system.tag.read('Path/mach_running').value == 1) and not (system.tag.read('Path/mach_stopped').value==1):
-		if system.tag.read('Path/manual_mode').value == 1:
-			tagPaths = ['Path/po_fiberID','Path/po_serID', 'Path/TU/tu_serID_manual', 'Path/TU/tu_fiberID']
-		else:
-			tagPaths = ['Path/po_fiberID','Path/po_serID', 'Path/TU/tu_serID', 'Path/TU/tu_fiberID']
+	if tapingDone == True:#to eliminate pf_Tension outliers before taping. added 2.5.19 PPC
+
+		while  (system.tag.read('Path/mach_running').value == 1) and not (system.tag.read('Path/mach_stopped').value==1):
+			if system.tag.read('Path/manual_mode').value == 1:
+				tagPaths = ['Path/po_fiberID','Path/po_serID', 'Path/TU/tu_serID_manual', 'Path/TU/tu_fiberID']
+			else:
+				tagPaths = ['Path/po_fiberID','Path/po_serID', 'Path/TU/tu_serID', 'Path/TU/tu_fiberID']
+				
+			list = system.tag.readAll(tagPaths)
+			pf_ten = ((system.tag.read('Path/pf_ten_meas').value))
+			tu_ten = ((system.tag.read('Path/TU/tu_ten_meas').value))
+			speed = ((system.tag.read('Path/mach_speed').value))
 			
-		list = system.tag.readAll(tagPaths)
-		pf_ten = ((system.tag.read('Path/pf_ten_meas').value))
-		tu_ten = ((system.tag.read('Path/TU/tu_ten_meas').value))
-		speed = ((system.tag.read('Path/mach_speed').value))
-		
-		timestamp = (strftime("%Y-%m-%d %H:%M:%S"))
-		pf_ten_array.append(pf_ten)
-		tu_ten_array.append(tu_ten)
-		speed_array.append(speed)
-		print speed
-		
-		#insert into the database = ten_log ---> changed table name to tension_log 12/3/18 PPC
-		system.db.runPrepUpdate("INSERT INTO tension_log (po_serialID, po_fiberID, tu_serialID, tu_rwrID, PF_TEN,TU_TEN, mach_speeds, timestamp ) VALUES (?,?,?,?,?,?,?,?)",[list[0].value,list[1].value,list[2].value,list[3].value, pf_ten, tu_ten, speed, timestamp],'mysql') 
-		
-		time.sleep(3)
+			timestamp = (strftime("%Y-%m-%d %H:%M:%S"))
+			pf_ten_array.append(pf_ten)
+			tu_ten_array.append(tu_ten)
+			speed_array.append(speed)
+			print speed
+			
+			#insert into the database = ten_log ---> changed table name to tension_log 12/3/18 PPC
+			system.db.runPrepUpdate("INSERT INTO tension_log (po_serialID, po_fiberID, tu_serialID, tu_rwrID, PF_TEN,TU_TEN, mach_speeds, timestamp ) VALUES (?,?,?,?,?,?,?,?)",[list[0].value,list[1].value,list[2].value,list[3].value, pf_ten, tu_ten, speed, timestamp],'mysql') 
+			
+			time.sleep(3)
 		
 		#next_value = system.tag.read('fDancerPosMe').value
 		
